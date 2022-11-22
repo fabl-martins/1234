14a09e0efceed23567da3ae41c115ff749ecda04
fablmartins <100200572+fablmartins@users.noreply.github.com>
2022-11-22 14:11:22 +0000

    hi

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../characters-details/characters-details.module').then( m => m.CharactersDetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'death-count',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../death-count/death-count.module').then( m => m.DeathCountPageModule)
+          }
+        ]
+      },
+      {
+        path: 'episodes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../episodes-details/episodes-details.module').then( m => m.EpisodesDetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'quotes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/characters',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
